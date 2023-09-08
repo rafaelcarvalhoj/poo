153ffb4a34f2305882160ea8f2b472630a4ff79a
Rafael Carvalho <105162671+rafaelcarvalhoj@users.noreply.github.com>
2023-09-08 20:15:34 +0000

    Atividade 1: Criando classes para um espaço geométrico

---
+++ Aula_1/espaco.py
@@ -0,0 +1,63 @@
+#Ponto
+class Ponto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def coordenadaFormated(self):
+        return f'{self.x}, {self.y}'
+    
+    def coordenada(self):
+        return [self.x, self.y]
+    
+#Triangulo
+class Triangulo():
+    def __init__(self, p1, p2, p3):
+        self.p1 = Ponto(p1[0], p1[1])
+        self.p2 = Ponto(p2[0], p2[1])
+        self.p3 = Ponto(p3[0], p3[1])
+    
+    def coordenadaFormated(self):
+        return f'Ponto 1: ({self.p1.coordenadaFormated()}) | Ponto 2: ({self.p2.coordenadaFormated()}) | Ponto 3: ({self.p3.coordenadaFormated()})'
+
+    def coordenada(self):
+        return [self.p1.coordenada, self.p2.coordenada, self.p3.coordenada]
+
+#Quadrado
+class Quadrado():
+    def __init__(self, p1, p2, p3, p4):
+        self.p1 = Ponto(p1[0], p1[1])
+        self.p2 = Ponto(p2[0], p2[1])
+        self.p3 = Ponto(p3[0], p3[1])
+        self.p4 = Ponto(p4[0], p4[1])
+        
+    def coordenadaFormated(self):
+        return f'Ponto 1: ({self.p1.coordenadaFormated()}) | Ponto 2: ({self.p2.coordenadaFormated()}) | Ponto 3: ({self.p3.coordenadaFormated()} | Ponto 4: ({self.p4.coordenadaFormated()})'
+    
+    def coordenada(self):
+        return [self.p1.coordenada, self.p2.coordenada, self.p3.coordenada, self.p4.coordenada]
+
+#Circulo
+class Circulo():
+    def __init__(self, p, raio):
+        self.p = Ponto(p[0], p[1])
+        self.raio = raio
+        
+    def coordenadaFormated(self):
+        return f'Meio: ({self.p.coordenadaFormated()}) | Raio: {self.raio}'
+    
+    def coordenada(self):
+        return [self.p.coordenada, self.raio]
+    
+# Reta
+class Reta():
+    def __init__(self, p1, p2):
+        self.p1 = Ponto(p1[0], p1[1])
+        self.p2 = Ponto(p2[0], p2[1])
+    
+    def coordenadaFormated(self):
+        return f'Ponto 1: ({self.p1.coordenadaFormated()}) | Ponto 2: ({self.p2.coordenadaFormated()})'
+
+    def coordenada(self):
+        return [self.p1.coordenada, self.p2.coordenada]
+    
