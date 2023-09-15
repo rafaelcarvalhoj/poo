d8340f9d0fe9a19b3aaf170cd8e22d12f0b340ee
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2023-09-15 19:52:13 -0300

    Adicionando diretorio para trabalho

---
+++ WorkSpace/Projeto_1/classe_1.py
@@ -0,0 +1,64 @@
+""""
+class Soma:
+    variavel A
+    variavel B
+    
+    funcao1(A, B):
+        definir A, B
+    
+    funcao2:
+        retorna A+B
+
+
+bob = Soma
+
+bob.funcao1(5, 10)
+
+resultado = bob.funcao2
+
+class Pato:
+    variavel tamanho
+    variavel cor
+    variavel peso
+    variavel raça
+
+    funcao falar:
+    
+    funcao nadar:
+    
+    funcao andar:
+    
+    funcao bicar:
+
+class Ponto:
+    variavel x
+    variavel y
+    
+    funcao1(x, y):
+        define x, y
+        
+    funcao2(x, y):
+        retorna (x, y)
+        
+        
+class Linha:
+    variavel p1 = Ponto
+    variavel p2 = Ponto
+    
+    funcao1(p1, p2):
+        p1.funcao1(p1.x p1.y)
+        p2.funcao1(p2.x p2.y)
+        
+    funcao2(p1):
+        retorna p1.funcao2
+        
+    funcao3(p2)
+        retorna p2.funcao2
+        
+class Triangulo:
+    variavel p1 = Ponto
+    variavel p2 = Ponto
+    variavel p3 = Ponto
+
+
+"""
